bfa88e9a94f190f913b310f3b49ce64493896406
Your Name <eirbjo@gmail.com>
2013-03-04 13:53:08 +0100

    Slow version of getBlogPosts

--- src/main/java/no/kantega/blog/dao/BlogDao.java
+++ src/main/java/no/kantega/blog/dao/BlogDao.java
@@ -91,23 +91,46 @@ public class BlogDao {
     }
 
     private List<BlogPost> getBlogPosts(String whereClaus, Object... parameters) {
-        String sql = "select blog.blogid, blog.blogname, blog.color, blogpost.blogpostid, blogpost.posttitle, blogpost.postcontent, blogpost.publishdate, " +
-                " (select count(*) from blogpostcomment where blogpostcomment.blogpostcommentid=blogpost.blogpostid) as commentcount " +
-                " from blogpost left join blog on blog.blogid=blogpost.blogid " + whereClaus + " order by  blogpost.blogpostid desc";
+        boolean fast = false;
+        if(fast) {
+            String sql = "select blog.blogid, blog.blogname, blog.color, blogpost.blogpostid, blogpost.posttitle, blogpost.postcontent, blogpost.publishdate, " +
+                    " (select count(*) from blogpostcomment where blogpostcomment.blogpostcommentid=blogpost.blogpostid) as commentcount " +
+                    " from blogpost left join blog on blog.blogid=blogpost.blogid " + whereClaus + " order by  blogpost.blogpostid desc";
 
-        return template.query(sql, new RowMapper<BlogPost>() {
-                    @Override
-                    public BlogPost mapRow(ResultSet rs, int i) throws SQLException {
-                        Blog blog = getBlogFromResultSet(rs);
-                        BlogPost post = new BlogPost(blog);
-                        post.setBlogPostId(rs.getInt("blogpostid"));
-                        post.setTitle(rs.getString("posttitle"));
-                        post.setContent(rs.getString("postcontent"));
-                        post.setPublishDate(rs.getTimestamp("publishdate"));
-                        post.setCommentCount(rs.getInt("commentcount"));
-                        return post;
-                    }
-                }, parameters);
+            return template.query(sql, new RowMapper<BlogPost>() {
+                @Override
+                public BlogPost mapRow(ResultSet rs, int i) throws SQLException {
+                    Blog blog = getBlogFromResultSet(rs);
+                    BlogPost post = new BlogPost(blog);
+                    post.setBlogPostId(rs.getInt("blogpostid"));
+                    post.setTitle(rs.getString("posttitle"));
+                    post.setContent(rs.getString("postcontent"));
+                    post.setPublishDate(rs.getTimestamp("publishdate"));
+                    post.setCommentCount(rs.getInt("commentcount"));
+                    return post;
+                }
+            }, parameters);
+
+        } else {
+            String sql = "select blogpost.blogid, blogpost.blogpostid, blogpost.posttitle, blogpost.postcontent, blogpost.publishdate, " +
+                    " (select count(*) from blogpostcomment where blogpostcomment.blogpostcommentid=blogpost.blogpostid) as commentcount " +
+                    " from blogpost " + whereClaus + " order by  blogpost.blogpostid desc";
+
+            return template.query(sql, new RowMapper<BlogPost>() {
+                @Override
+                public BlogPost mapRow(ResultSet rs, int i) throws SQLException {
+                    Blog blog = getBlogsWhere("where blogid=?", rs.getInt("blogid")).iterator().next();
+                    BlogPost post = new BlogPost(blog);
+                    post.setBlogPostId(rs.getInt("blogpostid"));
+                    post.setTitle(rs.getString("posttitle"));
+                    post.setContent(rs.getString("postcontent"));
+                    post.setPublishDate(rs.getTimestamp("publishdate"));
+                    post.setCommentCount(rs.getInt("commentcount"));
+                    return post;
+                }
+            }, parameters);
+
+        }
     }
 
     public BlogPost getBlogPost(long blogPostId) {
