c8e121c838ae373ed41c2769f73a77c3b91f9416
austvik <jaustvik@acm.org>
2013-03-07 15:04:52 +0100

    Documentation and Java 7 database magic

--- src/main/java/no/kantega/blog/initializer/BlogInitializer.java
+++ src/main/java/no/kantega/blog/initializer/BlogInitializer.java
@@ -24,20 +24,26 @@ import static no.kantega.blog.services.Services.addService;
 import static no.kantega.blog.services.Services.getService;
 
 /**
- *
+ * Initializes the container.
  */
-public class BlogInitializer implements ServletContainerInitializer{
+public class BlogInitializer implements ServletContainerInitializer {
+    
+    private static final String DB_ALREADY_EXISTS = "X0Y32";
 
+    /**
+     * Called when the container starts.
+     * 
+     * @param classes Classes loaded
+     * @param servletContext Context we configure
+     * @throws ServletException In case we fail to start
+     */
     @Override
     public void onStartup(Set<Class<?>> classes, ServletContext servletContext) throws ServletException {
         Logger.getLogger(getClass().getName()).info("Starting up");
 
         configureSessionListener(servletContext);
-
         addService(DataSource.class, initializeDatasource(), servletContext);
-
         addService(BlogDao.class, new BlogDao(getService(DataSource.class, servletContext)), servletContext);
-
         configureCharEncFilter(servletContext);
     }
 
@@ -52,20 +58,31 @@ public class BlogInitializer implements ServletContainerInitializer{
         servletContext.addFilter("charEncoding", CharEncodingFilter.class).addMappingForUrlPatterns(enums, false, "*.jsp");
     }
 
+    /**
+     * Prepare data source (database connection).
+     * 
+     * @return The data source to use
+     * @throws ServletException In case we can't get a database connection
+     */
     private DataSource initializeDatasource() throws ServletException {
-
-
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(ClientDriver40.class.getName());
         dataSource.setUrl("jdbc:derby://localhost:1527/blogdb;create=true");
 
-
         //dataSource.setMaxActive(50);
 
-        Connection connection = null;
-        try {
-            connection = dataSource.getConnection();
-            Statement statement = connection.createStatement();
+        createTables(dataSource);
+        return dataSource;
+    }
+
+    /**
+     * Create tables used in the application.
+     * 
+     * @param dataSource Data source to create them in
+     * @throws ServletException In case tables can't be created
+     */
+    private void createTables(DataSource dataSource) throws ServletException {
+        try (Connection connection = dataSource.getConnection()) {
             List<String> statements = Arrays.asList(
                     "create table blog (blogid  integer NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
                             " blogname varchar(255) NOT NULL UNIQUE, " +
@@ -84,24 +101,15 @@ public class BlogInitializer implements ServletContainerInitializer{
                                                 " commentpublishdate timestamp NOT NULL " +
                                                 " )");
 
-
-            for (String sql : statements) {
-                statement.execute(sql);
-            }
-
-        } catch (SQLException e) {
-            if(!"X0Y32".equals(e.getSQLState())) {
-                throw new ServletException(e);
-            }
-        } finally {
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    // Don't care
+            try (Statement statement = connection.createStatement()) {
+                for (String sql : statements) {
+                    statement.execute(sql);
                 }
             }
+        } catch (SQLException e) {
+            if (!DB_ALREADY_EXISTS.equals(e.getSQLState())) {
+                throw new ServletException(e);
+            }
         }
-        return dataSource;
     }
 }
