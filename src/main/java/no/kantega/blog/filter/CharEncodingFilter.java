95ca6cd5ba53fb98308b132ce74bea1d8ba8b87d
austvik <jaustvik@acm.org>
2013-03-07 15:04:15 +0100

    Documentation

--- src/main/java/no/kantega/blog/filter/CharEncodingFilter.java
+++ src/main/java/no/kantega/blog/filter/CharEncodingFilter.java
@@ -5,14 +5,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- *
+ * Set the content type on all callse.
  */
 public class CharEncodingFilter implements Filter {
+
+    /**
+     * Initialize the filter - not used.
+     * 
+     * @param filterConfig Configuration for the filter
+     * @throws ServletException In case the filter can't be configured
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
+    /**
+     * Set content type on the response.
+     */
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
@@ -20,8 +29,10 @@ public class CharEncodingFilter implements Filter {
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
+    /**
+     * Destroy filter on shutdown - not used.
+     */
     @Override
     public void destroy() {
-
     }
 }
