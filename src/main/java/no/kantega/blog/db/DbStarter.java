7a6419cd493fd5c1bb4b4ab43695423cface3362
austvik <jaustvik@acm.org>
2013-03-07 15:07:57 +0100

    Clean up code with doc

--- src/main/java/no/kantega/blog/db/DbStarter.java
+++ src/main/java/no/kantega/blog/db/DbStarter.java
@@ -7,31 +7,30 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 
 /**
- *
+ * Class that gives a simple way to start and stop the database.
  */
-public class DbStarter {
+public final class DbStarter {
 
+    private static final int PORT_NUMBER = 1527;
+    
+    private DbStarter() {}
+    
     public static void main(String[] args) throws Exception {
-
         System.setProperty("derby.system.home", "target");
-
         System.setProperty("visualvm.display.name", "TechBlogDB");
+
         System.out.println("Starting database");
 
-        NetworkServerControl server = new NetworkServerControl(InetAddress.getByName("localhost"),1527);
-
-
+        NetworkServerControl server = new NetworkServerControl(InetAddress.getByName("localhost"), PORT_NUMBER);
         server.start(null);
 
         //server.setMaxThreads(5);
 
-
         System.out.println("Database server started, press ENTER to shut down");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
         br.readLine();
 
         server.shutdown();
-
 
         System.out.println("Database server successfully shut down");
     }
