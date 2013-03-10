d7729876c5b55e2dea389b9bb73fd34d16086fee
austvik <jaustvik@acm.org>
2013-03-10 17:19:00 +0100

    Remove leakage

--- src/main/java/no/kantega/blog/listener/BlogSessionListener.java
+++ src/main/java/no/kantega/blog/listener/BlogSessionListener.java
@@ -1,19 +1,10 @@
 package no.kantega.blog.listener;
 
-import no.kantega.blog.dao.BlogDao;
-import no.kantega.blog.model.Blog;
-import no.kantega.blog.model.BlogPost;
-
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import no.kantega.blog.dao.BlogPostDao;
-
-import static no.kantega.blog.services.Services.getService;
 
 /**
  * Listen to created and destroyed sessions.
@@ -29,31 +20,11 @@ public class BlogSessionListener implements HttpSessionListener {
      */
     @Override
     public void sessionCreated(HttpSessionEvent sessionEvent) {
-
         HttpSession session = sessionEvent.getSession();
-        try {
-            BlogDao dao = getService(BlogDao.class, session.getServletContext());
-            BlogPostDao postDao = getService(BlogPostDao.class, session.getServletContext());
-
-            Map<String, BlogPost> blogPostCache = new HashMap<>();
-
-            for(Blog blog : dao.getAllBlogs()) {
-                for(BlogPost post : postDao.getBlogPosts(blog)) {
-                    blogPostCache.put(blog.getLinkId() +"/" + post.getLinkId(), post);
-                }
-            }
-
-            session.setAttribute("blogPostCache", blogPostCache);
-
-            // 8 hours sessions
-            session.setMaxInactiveInterval(8 * 60 * 60);
-        } finally {
-            sessions.put(session.getId(), session);
-            getTotalSessionCount.incrementAndGet();
-        }
+        session.setMaxInactiveInterval(8 * 60 * 60); // 8 hours sessions
+        sessions.put(session.getId(), session);
+        getTotalSessionCount.incrementAndGet();
     }
-
-
 
     /**
      * Called when a session is destroyed/invalidated (e.g. timeout).
